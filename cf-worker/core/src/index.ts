7496b094b81a43cef35c0ef87a1839d7291f8d5e
chyyynh <chyyynh@users.noreply.github.com>
2026-03-17 14:33:56 +0000

    Update content

--- cf-worker/core/src/index.ts
+++ cf-worker/core/src/index.ts
@@ -72,7 +72,7 @@ export default {
 		logInfo('CORE', 'Scheduled', { cron: event.cron });
 
 		if (event.cron === '*/5 * * * *') ctx.waitUntil(handleRSSCron(env, ctx));
-		else if (event.cron === '0 * * * *') ctx.waitUntil(handleTwitterCron(env, ctx));
+		else if (event.cron === '0 */6 * * *') ctx.waitUntil(handleTwitterCron(env, ctx));
 		else if (event.cron === '0 3 * * *') ctx.waitUntil(handleRetryCron(env, ctx));
 	},
 
